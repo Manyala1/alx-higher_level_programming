432dfade0933f5f42b01d2735aa7b9d4b553817f
Paul <paulmuthama60@gmail.com>
2023-12-01 06:37:05 -0800

    ./3-infinite_add.py 79 10 -40 -300 89

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,13 +4,13 @@ import sys
 if __name__ != "__main__":
     exit()
 
-    arg = len(sys.argv) -1
+argc = len(sys.argv) - 1
 
-    i = 0
-    result =0
-    for arg in sys.argv:
-        if i != 0:
-            result += int(arg)
-        else:
-            i += 1
-            print("{:d}".format(reslt))
+i = 0
+result = 0
+for arg in sys.argv:
+    if i != 0:
+        result += int(arg)
+    else:
+        i += 1
+print("{:d}".format(result))
