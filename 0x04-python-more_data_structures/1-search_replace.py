46572a118edfa75572ad1123574fe419015cca30
Paul <paulmuthama60@gmail.com>
2023-12-07 02:45:14 -0800

    ./1-main.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = []
-    for element in my_list:
-        if element == search:
-            new_list.append(replace)
-        else:
-            new_list.append(element)
-        return new_list
+  """
+  Replaces all occurrences of `search` element with `replace` element in a new list.
+
+  Args:
+      my_list: A list of any type of elements.
+      search: The element to be replaced.
+      replace: The new element to replace with.
+
+  Returns:
+      A new list with all occurrences of `search` replaced by `replace`.
+  """
+  new_list = []
+  for element in my_list:
+    if element == search:
+      new_list.append(replace)
+    else:
+      new_list.append(element)
+  return new_list
