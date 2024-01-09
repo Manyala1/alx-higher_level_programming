bbae54275d85dcaf1e4744e086e2e4681fb3ed11
Paul <paulmuthama60@gmail.com>
2024-01-09 06:19:48 -0800

    ./4-main.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module to check if an instance of a class that inherited (directly or idirectly)
+from the specified class 
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    True if object is an instance of a classthat inherited
+    (directlt/indirectly) from the specified class
+    """
+    if type)(obj) == a_class:
+        return False
+    return isintance(obj, a_class)
