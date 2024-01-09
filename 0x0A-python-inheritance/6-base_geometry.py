59f49de06bdf08ba0cf199c4220cad9cf8d75ea8
Paul <paulmuthama60@gmail.com>
2024-01-09 10:28:59 -0800

    ./6-main.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+module for BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    still empty, area not implemented
+    """
+    # public instance method
+    def area(self):
+        raise Exception("area() is not implemented")
