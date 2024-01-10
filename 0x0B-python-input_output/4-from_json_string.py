718e331d606604512cf2413c01e0638aa562043e
Paul <paulmuthama60@gmail.com>
2024-01-10 14:08:55 -0800

    ./4-main.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+# 6-from_json_string.py
 """Defines a string-to-JSON function"""
 import json
 
 def from_json_string(my_str):
-    my_str == Python data structure:
-        """Return the JSON representation of my_str"""
-        return json.dumps(my_str)
+        """Return the python object representation of a JSON string"""
+        return json.loads(my_str)
