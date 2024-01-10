c7afa04372e17b3aaf0c09aff7cb72342764e539
Paul <paulmuthama60@gmail.com>
2024-01-10 14:27:55 -0800

    6-load_from_json_file.py

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a JSON file-reading function"""
+import json
+
+
+
+def load_from_json_file(filename):
+    """creates a python object from a JSON file"""
+    return json.load(f)
