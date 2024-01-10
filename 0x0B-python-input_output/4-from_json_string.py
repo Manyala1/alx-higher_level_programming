af3eda54dddf43f7cc4694fdb4d14c818afdfd37
Paul <paulmuthama60@gmail.com>
2024-01-10 14:05:31 -0800

    ./4-main.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a string-to-JSON function"""
+import json
+
+def from_json_string(my_str):
+    my_str == Python data structure:
+        """Return the JSON representation of my_str"""
+        return json.dumps(my_str)
