3330ee0da3e566c4cf59f487e656eb6959479c10
manyala paul <paulmuthama60@gmail.com>
2024-03-18 13:46:23 -0700

    kindly

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,5 +1,2 @@
--- Creates the table force_name
-CREATE TABLE IF NOT EXISTS 'force_name' (
-	'id' INT,
-	'name' VARCHAR(256) NOT NULL
-	);
+-- Creates the table forcr_name on my mysql server
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
