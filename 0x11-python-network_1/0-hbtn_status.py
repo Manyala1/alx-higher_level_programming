57e317671013540b7cf37ecf32d71863c8aed336
manyala <paulmuthama60@gmail.com>
2024-07-06 10:25:44 -0700

    push

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-
+"module that fetches https://alx-intranet.hbtn.io/status"
 import urllib.request
-url = 'https://alx-intranet.hbtn.io/status'
-with urllib.request.urlopen(url) as response:
-    body = response.read().decode('utf-8')
-    print("Body responce:")
-    print(f"\t- {body}")
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        print('Body response:')
+        print('\t- type: {}', format(type(html)))
+        print('\t- content: {}', format(html))
+        print('\t- utf8 content: {}', format(html.decode("utf-8")))
