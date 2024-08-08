6d6eac03e7d92a2d4ca8d46d154a3d579ab81412
Manyala1 <paulmuthama60@gmail.com>
2024-08-08 05:29:23 -0700

    script 3

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,8 @@
+<script>
+	// Use jQuery to change the text color of the <header> element when the user clicks the div
+	$(documet).ready(function() {
+		$('#red_header').click(function() {
+			$('header').css('color', '#FF0000');
+		});
+	})'
+</script>
---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,6 @@
+consct $header = $('header');
+const $divDeadHeader = $('div#red_header');
+
+$divRedHeader.on('click', function () {
+	$headerElem.addClass('red');
+});
