4460914305980294a22c6bc3af3cee8e1831430f
Manyala1 <paulmuthama60@gmail.com>
2024-08-08 01:18:36 -0700

    script

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,6 @@
+<script>
+	// select the <header> element and update its text color to red
+	document.addEventListener("DOMCotentLoaded", function() {
+		document.querySelector('header').style.color =  'FF0000';
+	});
+</script>
