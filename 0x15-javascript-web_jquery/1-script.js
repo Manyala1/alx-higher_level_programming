95bbbc84652cc9566946eeffdb4abf6ccbe977b7
Manyala1 <paulmuthama60@gmail.com>
2024-08-08 01:28:13 -0700

    script2

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,6 @@
+<scrip>
+	// a JavaScript script that updates the text color of the <header> element to red (#FF0000) using jquery api
+	$(document).ready(function() {
+		$('header').css('color;, '#FF0000')
+	});
+</script>
